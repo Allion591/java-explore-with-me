3f43b48ac1bbcb5360f9280289219491d092b5fc
Allion591 <leo59158@yandex.ru>
2025-11-07 20:57:47 +0500

    Реализован класс коннектор для передачи и получения запросов с сервисом статистики

--- main-service/src/main/java/ru/practicum/main/stat/ConnectionToStatistics.java
+++ main-service/src/main/java/ru/practicum/main/stat/ConnectionToStatistics.java
@@ -1,4 +1,12 @@
 package ru.practicum.main.stat;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
 public interface ConnectionToStatistics {
+
+    void postHit(HttpServletRequest request);
+
+    Map<String, Long> getViews(List<String> uris);
 }
