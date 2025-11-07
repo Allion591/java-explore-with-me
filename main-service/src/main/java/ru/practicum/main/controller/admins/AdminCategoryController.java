d738aef326c4d27b84ec1626b00a5ac5c4250df5
Allion591 <leo59158@yandex.ru>
2025-11-07 20:39:49 +0500

    Добавлены контроллеры для Админ энпоинтов

--- main-service/src/main/java/ru/practicum/main/controller/admins/AdminCategoryController.java
+++ main-service/src/main/java/ru/practicum/main/controller/admins/AdminCategoryController.java
@@ -1,4 +1,43 @@
 package ru.practicum.main.controller.admins;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.main.dto.category.CategoryDto;
+import ru.practicum.main.dto.category.NewCategoryDto;
+import ru.practicum.main.service.interfaces.CategoryService;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin/categories")
+@RequiredArgsConstructor
+@Validated
 public class AdminCategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDto createCategory(@Valid @RequestBody NewCategoryDto newCategoryDto) {
+        log.info("Admin: создание категории {}", newCategoryDto);
+        return categoryService.createCategory(newCategoryDto);
+    }
+
+    @DeleteMapping("/{catId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable @Min(1) Long catId) {
+        log.info("Admin: удаление категории с id={}", catId);
+        categoryService.deleteCategory(catId);
+    }
+
+    @PatchMapping("/{catId}")
+    public CategoryDto updateCategory(@PathVariable @Min(1) Long catId,
+                                      @Valid @RequestBody CategoryDto categoryDto) {
+        log.info("Admin: обновление категории с id={} на {}", catId, categoryDto);
+        return categoryService.updateCategory(catId, categoryDto);
+    }
 }
--- main-service/src/main/java/ru/practicum/main/controller/admins/AdminCompilationController.java
+++ main-service/src/main/java/ru/practicum/main/controller/admins/AdminCompilationController.java
@@ -1,4 +1,44 @@
 package ru.practicum.main.controller.admins;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.main.dto.compilation.CompilationDto;
+import ru.practicum.main.dto.compilation.NewCompilationDto;
+import ru.practicum.main.dto.compilation.UpdateCompilationRequest;
+import ru.practicum.main.service.interfaces.CompilationService;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin/compilations")
+@RequiredArgsConstructor
+@Validated
 public class AdminCompilationController {
+
+    private final CompilationService compilationService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CompilationDto createCompilation(@Valid @RequestBody NewCompilationDto newCompilationDto) {
+        log.info("Admin: создание подборки с названием: {}", newCompilationDto.getTitle());
+        return compilationService.createCompilation(newCompilationDto);
+    }
+
+    @DeleteMapping("/{compId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCompilation(@PathVariable @Min(1) Long compId) {
+        log.info("Admin: удаление подборки с id={}", compId);
+        compilationService.deleteCompilation(compId);
+    }
+
+    @PatchMapping("/{compId}")
+    public CompilationDto updateCompilation(@PathVariable @Min(1) Long compId,
+                                            @Valid @RequestBody UpdateCompilationRequest updateCompilationRequest) {
+        log.info("Admin: обновление подборки с id={}", compId);
+        return compilationService.updateCompilation(compId, updateCompilationRequest);
+    }
 }
