ea07d1b5adb630703ea38e92b8252a8bd9a6cee0
Allion591 <leo59158@yandex.ru>
2025-11-07 20:45:21 +0500

    Реализованы DTO для событий

--- main-service/src/main/java/ru/practicum/main/dto/event/EventFullDto.java
+++ main-service/src/main/java/ru/practicum/main/dto/event/EventFullDto.java
@@ -1,4 +1,51 @@
 package ru.practicum.main.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.main.dto.category.CategoryDto;
+import ru.practicum.main.dto.location.LocationDto;
+import ru.practicum.main.dto.user.UserShortDto;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventFullDto {
+    private Long id;
+
+    private String annotation;
+
+    private CategoryDto category;
+    private Long confirmedRequests;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdOn;
+
+    private String description;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime eventDate;
+
+    private UserShortDto initiator;
+    private LocationDto location;
+    private Boolean paid;
+
+    @JsonProperty("participantLimit")
+    @Builder.Default
+    private Integer participantLimit = 0;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime publishedOn;
+
+    private Boolean requestModeration;
+    private String state;
+
+    private String title;
+
+    private Long views;
 }
--- main-service/src/main/java/ru/practicum/main/dto/event/UpdateEventUserRequest.java
+++ main-service/src/main/java/ru/practicum/main/dto/event/UpdateEventUserRequest.java
@@ -1,4 +1,45 @@
 package ru.practicum.main.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.main.dto.location.LocationDto;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateEventUserRequest {
+    @Size(min = 20, max = 2000, message = "Аннотация не может быть меньше 20ти или больше 2000 символов")
+    private String annotation;
+
+    private Long category;
+
+    @Size(min = 20, max = 7000, message = "Описание не может быть меньше 20ти или больше 7000 символов")
+    private String description;
+
+    @Future(message = "Дата начала события должна быть в будущем")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime eventDate;
+
+    private LocationDto location;
+    private Boolean paid;
+
+    @Min(value = 0, message = "Количество участников должно быть положительным")
+    @JsonProperty("participantLimit")
+    private Integer participantLimit;
+
+    @Builder.Default
+    private Boolean requestModeration = true;
+    private String stateAction;
+
+    @Size(min = 3, max = 120, message = "Заголовок не может быть меньше 3х или больше 120 символов")
+    private String title;
 }
