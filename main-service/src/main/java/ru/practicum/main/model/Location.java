889325c5e5476595d8944814054f02bc25f429fa
Allion591 <leo59158@yandex.ru>
2025-11-07 20:53:17 +0500

    Реализованы модели для хранения в БД

--- main-service/src/main/java/ru/practicum/main/model/Location.java
+++ main-service/src/main/java/ru/practicum/main/model/Location.java
@@ -1,4 +1,21 @@
 package ru.practicum.main.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Location {
+    @Column(name = "lat", nullable = false)
+    private Float lat;
+
+    @Column(name = "lon", nullable = false)
+    private Float lon;
 }
