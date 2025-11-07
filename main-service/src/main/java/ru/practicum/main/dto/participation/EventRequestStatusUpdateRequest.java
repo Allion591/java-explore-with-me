9799cd84f39975c99e3c18f8be9fca71b492349d
Allion591 <leo59158@yandex.ru>
2025-11-07 20:48:41 +0500

    Реализованы DTO для передачи и хранения участников

--- main-service/src/main/java/ru/practicum/main/dto/participation/EventRequestStatusUpdateRequest.java
+++ main-service/src/main/java/ru/practicum/main/dto/participation/EventRequestStatusUpdateRequest.java
@@ -1,4 +1,21 @@
 package ru.practicum.main.dto.participation;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventRequestStatusUpdateRequest {
+    @NotEmpty(message = "Список запросов не может быть пустым")
+    private List<Long> requestIds;
+
+    @NotNull(message = "Статус не может быть пустым")
+    private String status;
 }
--- main-service/src/main/java/ru/practicum/main/dto/participation/EventRequestStatusUpdateResult.java
+++ main-service/src/main/java/ru/practicum/main/dto/participation/EventRequestStatusUpdateResult.java
@@ -1,4 +1,20 @@
 package ru.practicum.main.dto.participation;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventRequestStatusUpdateResult {
+    @Builder.Default
+    private List<ParticipationRequestDto> confirmedRequests = new ArrayList<>();
+
+    @Builder.Default
+    private List<ParticipationRequestDto> rejectedRequests = new ArrayList<>();
 }
