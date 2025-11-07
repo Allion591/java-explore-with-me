f0cfeb973c2cff4ac139227c4b61ff74beb47638
Allion591 <leo59158@yandex.ru>
2025-11-07 20:56:07 +0500

    Реализованы интерфейсы сервисы для поддержеи многослойности проекта

--- main-service/src/main/java/ru/practicum/main/service/interfaces/CompilationService.java
+++ main-service/src/main/java/ru/practicum/main/service/interfaces/CompilationService.java
@@ -1,4 +1,20 @@
 package ru.practicum.main.service.interfaces;
 
+import ru.practicum.main.dto.compilation.CompilationDto;
+import ru.practicum.main.dto.compilation.NewCompilationDto;
+import ru.practicum.main.dto.compilation.UpdateCompilationRequest;
+
+import java.util.List;
+
 public interface CompilationService {
+
+    CompilationDto createCompilation(NewCompilationDto newCompilationDto);
+
+    void deleteCompilation(Long compilationId);
+
+    CompilationDto updateCompilation(Long compilationId, UpdateCompilationRequest updateRequest);
+
+    List<CompilationDto> getCompilations(Boolean pinned, Integer from, Integer size);
+
+    CompilationDto getCompilation(Long compilationId);
 }
--- main-service/src/main/java/ru/practicum/main/service/interfaces/EventService.java
+++ main-service/src/main/java/ru/practicum/main/service/interfaces/EventService.java
@@ -1,4 +1,29 @@
 package ru.practicum.main.service.interfaces;
 
+import ru.practicum.main.dto.event.*;
+import ru.practicum.main.dto.filter.AdminEventFilterParams;
+import ru.practicum.main.dto.filter.EventPublicFilterRequest;
+import ru.practicum.main.model.Event;
+
+import java.util.List;
+
 public interface EventService {
+
+    EventFullDto createEvent(Long userId, NewEventDto newEventDto);
+
+    List<EventShortDto> getUserEvents(Long userId, Integer from, Integer size);
+
+    EventFullDto getUserEvent(Long userId, Long eventId);
+
+    EventFullDto updateEventByUser(Long userId, Long eventId, UpdateEventUserRequest updateRequest);
+
+    List<EventFullDto> getEventsByAdmin(AdminEventFilterParams filterParams);
+
+    EventFullDto updateEventByAdmin(Long eventId, UpdateEventAdminRequest updateRequest);
+
+    List<EventShortDto> getEventsPublic(EventPublicFilterRequest filter);
+
+    EventFullDto getEventPublic(Long eventId);
+
+    Event getEventById(Long eventId);
 }
