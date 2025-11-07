29fe164db546b27ba0497fd46594556a0bfaec9d
Allion591 <leo59158@yandex.ru>
2025-11-07 21:02:51 +0500

    Создание файлов Pom, Schema, Главного класса для запуска приложения, application-ptoperties и их настройка

--- main-service/src/main/java/ru/practicum/main/MainServiceStart.java
+++ main-service/src/main/java/ru/practicum/main/MainServiceStart.java
@@ -1,2 +1,13 @@
+package ru.practicum.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"ru.practicum.main", "ru.practicum.stats"})
 public class MainServiceStart {
+    public static void main(String[] args) {
+        SpringApplication.run(MainServiceStart.class, args);
+    }
 }
