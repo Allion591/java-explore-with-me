8ac272d14a8ff77516891ae9c1c5e0dc5ba1703a
Allion591 <leo59158@yandex.ru>
2025-11-07 20:43:29 +0500

    Реализованы DTO для подборок событий

--- main-service/src/main/java/ru/practicum/main/dto/compilation/UpdateCompilationRequest.java
+++ main-service/src/main/java/ru/practicum/main/dto/compilation/UpdateCompilationRequest.java
@@ -1,4 +1,22 @@
 package ru.practicum.main.dto.compilation;
 
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateCompilationRequest {
+    private List<Long> events;
+
+    @Builder.Default
+    private Boolean pinned = false;
+
+    @Size(max = 50, message = "Длина заголовка подборки не может быть больше 50ти симвлов")
+    private String title;
 }
