3559f90bc50266ea290f558f48e754179e7d344b
Allion591 <leo59158@yandex.ru>
2025-11-14 15:37:41 +0500

    Реализовал класс enum для статусов комментариев

--- main-service/src/main/java/ru/practicum/main/enums/CommentStatus.java
+++ main-service/src/main/java/ru/practicum/main/enums/CommentStatus.java
@@ -1,4 +1,9 @@
 package ru.practicum.main.enums;
 
 public enum CommentStatus {
+    PENDING,      // Ожидает модерации
+    APPROVED,     // Одобрен, виден всем
+    REJECTED,     // Отклонен модератором
+    DELETED,      // Удален
+    EDITED,       // Отредактирован, требует повторной модерации
 }
