e9fa72f9f18034bbbc1664ef39cdb8a1aad843ea
Allion591 <leo59158@yandex.ru>
2025-11-14 15:40:26 +0500

    Реализовал сущность Comment

--- main-service/src/main/java/ru/practicum/main/model/Comment.java
+++ main-service/src/main/java/ru/practicum/main/model/Comment.java
@@ -1,4 +1,49 @@
 package ru.practicum.main.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import ru.practicum.main.enums.CommentStatus;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "comments")
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comments_id")
+    private Long id;
+
+    @Column(name = "text", nullable = false, length = 255)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state", nullable = false, length = 20)
+    @Builder.Default
+    private CommentStatus state = CommentStatus.PENDING;
+
+    @CreationTimestamp
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime created;
+
+    @Column(name = "updated_date")
+    @Builder.Default
+    private LocalDateTime updated = LocalDateTime.now();
 }
