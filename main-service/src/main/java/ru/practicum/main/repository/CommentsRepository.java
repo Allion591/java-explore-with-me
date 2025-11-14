47ae3892040431b234e826e8cf214cb3136f68d2
Allion591 <leo59158@yandex.ru>
2025-11-14 15:41:33 +0500

    Добавил репозиторий для работы с комментариями

--- main-service/src/main/java/ru/practicum/main/repository/CommentsRepository.java
+++ main-service/src/main/java/ru/practicum/main/repository/CommentsRepository.java
@@ -1,4 +1,20 @@
 package ru.practicum.main.repository;
 
-public interface CommentsRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.practicum.main.enums.CommentStatus;
+import ru.practicum.main.model.Comment;
+
+import java.util.List;
+
+@Repository
+public interface CommentsRepository extends JpaRepository<Comment, Long> {
+
+    Page<Comment> findByStateIn(List<CommentStatus> states, Pageable pageable);
+
+    Page<Comment> findByAuthorId(Long authorId, Pageable pageable);
+
+    Page<Comment> findByEventIdAndState(Long eventId, CommentStatus state, Pageable pageable);
 }
